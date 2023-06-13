66aadd11020e90ee334a302cfb509a40797a7518
BIAsia <l980492289@163.com>
2023-06-13 19:51:06 +0800

    add gradient effect & basic controls

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 import * as THREE from 'three';
-import {Pane} from 'tweakpane';
+import { Pane } from 'tweakpane';
 import vertex from './shaders/vertex.glsl'
 import fragment from './shaders/fragment.glsl'
 
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 // let OrbitControls = require("three/examples/jsm/controls/OrbitControls").OrbitControls
 
-export default class Sketch{
-    constructor(){
+export default class Sketch {
+    constructor() {
         this.container = document.getElementById('container');
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
 
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize( this.width, this.height );
-        this.renderer.setClearColor(0x111111,1)
+        this.renderer.setSize(this.width, this.height);
+        this.renderer.setClearColor(0xefefef, 1)
         this.renderer.physicallyCorrectLights = true;
         this.renderer.outputEncoding = THREE.sRGBEncoding;
 
-        this.container.appendChild( this.renderer.domElement );
+        this.container.appendChild(this.renderer.domElement);
 
         this.camera = new THREE.PerspectiveCamera(
-            70, 
-            window.innerWidth / window.innerHeight, 
-            0.001, 
+            80,
+            window.innerWidth / window.innerHeight,
+            0.001,
             1000
         );
 
@@ -45,55 +45,94 @@ export default class Sketch{
         this.setupResize();
 
     }
-    isometricFill(){
+    isometricFill() {
         var frustumSize = 1;
-        var aspect = this.width/this.height;
+        var aspect = this.width / this.height;
         this.camera = new THREE.OrthographicCamera(
             frustumSize / -2,
             frustumSize / 2,
             frustumSize / 2,
             frustumSize / -2,
             -1000,
-            1000 
+            1000
         );
     }
 
-    settings(){
+    settings() {
         this.pane = new Pane();
-        this.PARAMS = {
-            progress: 0,
+        // this.PARAMS = {
+        //     progress: 0,
+        // };
+        // this.pane.addInput(
+        //     this.PARAMS, 'progress',
+        //     { min: 0, max: 1 }
+        // ).on('change', (ev) => {
+        //     this.material.uniforms.uProgress.value = ev.value;
+        // })
+
+        this.PARAMS_COLOR = {
+            colorA: "#0888B8",
+            colorB: "#0870A8",
+            colorC: "#f09878",
         };
-        this.pane.addInput(
-            this.PARAMS, 'progress',
-            {min: 0, max: 1}
-        ).on('change', (ev)=>{
-            this.material.uniforms.uProgress.value = ev.value;
-        })
+        const colorFolder = this.pane.addFolder({
+            title: 'Color',
+            expanded: true,
+        });
+        colorFolder.addInput(this.PARAMS_COLOR, 'colorA')
+        colorFolder.addInput(this.PARAMS_COLOR, 'colorB')
+        colorFolder.addInput(this.PARAMS_COLOR, 'colorC')
+
+        this.PARAMS = {
+            lightness: 0.1,
+        }
+        colorFolder.addInput(this.PARAMS, 'lightness', {min: -1, max: 1, step: 0.01})
+
     }
 
-    mouseEvent(){
-        document.addEventListener('mousemove', (e)=>{
+    mouseEvent() {
+        document.addEventListener('mousemove', (e) => {
             // mousemove
         })
     }
 
-    addPost(){
+    addPost() {
 
     }
 
-    addMesh(){
-        this.geometry = new THREE.PlaneBufferGeometry(1,1);
+    addMesh() {
+        this.geometry = new THREE.PlaneBufferGeometry(10, 10, 192, 192);
 
         this.material = new THREE.ShaderMaterial({
             vertexShader: vertex,
             fragmentShader: fragment,
-            
+
             uniforms: {
-                uMouse: {value: 0},
-                uResolution: {value: new THREE.Vector2()},
-                uProgress: {value: 0},
-                uImg: {value: this.texture},
-                uTime: {value: 0},
+                uMouse: { value: 0 },
+                uResolution: { value: new THREE.Vector2() },
+                uProgress: { value: 0 },
+                uImg: { value: this.texture },
+                uTime: { value: 0 },
+                uSpeed: { value: 0.01 },
+                uNoiseDensity: { value: 1.2 },
+                uNoiseStrength: { value: 1.4 },
+                diffuse: { value: 0.5 },
+                emissive: { value: 0 },
+                roughness: { value: 0 },
+                metalness: { value: 0 },
+                opacity: { value: 1 },
+                // uC1r: { value: rgbColors.uC1r },
+                // uC1g: { value: rgbColors.uC1g },
+                // uC1b: { value: rgbColors.uC1b },
+                // uC2r: { value: rgbColors.uC2r },
+                // uC2g: { value: rgbColors.uC2g },
+                // uC2b: { value: rgbColors.uC2b },
+                // uC3r: { value: rgbColors.uC3r },
+                // uC3g: { value: rgbColors.uC3g },
+                // uC3b: { value: rgbColors.uC3b },
+                uColor: {value: [ "#0888B8", "#0870A8", "#f09878"].map((color) => new THREE.Color(color))},
+                uLightness: {value: 0.12},
+
                 // uSize: {value: 6.0},
                 // uScale: {value: 0}
             },
@@ -101,16 +140,16 @@ export default class Sketch{
             // transparent: true,
             // wireframe: true,
         })
-        
-        this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.scene.add( this.mesh );
+
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.mesh);
     }
 
-    setupResize(){
+    setupResize() {
         window.addEventListener("resize", this.resize.bind(this));
     }
 
-    resize(){
+    resize() {
         this.width = this.container.offsetWidth;
         this.height = this.container.offsetHeight;
         this.renderer.setSize(this.width, this.height);
@@ -119,15 +158,19 @@ export default class Sketch{
         this.material.uniforms.uResolution.value.y = this.height;
     }
 
-    render(){
+    render() {
         this.time++;
-        
+        // console.log(rgbColors)
         // this.scene.rotation.x = this.time / 2000;
-	    // this.scene.rotation.y = this.time / 1000;
+        // this.scene.rotation.y = this.time / 1000;
         this.material.uniforms.uTime.value = this.time;
+        this.palette = Object.values(this.PARAMS_COLOR).map((color) => new THREE.Color(color))
+        this.material.uniforms.uColor.value = this.palette;
+        this.material.uniforms.uLightness.value = this.PARAMS.lightness;
+
         this.control.update();
-        this.renderer.render( this.scene, this.camera );
-        
+        this.renderer.render(this.scene, this.camera);
+
         window.requestAnimationFrame(this.render.bind(this))
     }
 }
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,12 +1,48 @@
-varying vec3 vPosition;
-varying vec2 vUV;
-varying vec3 vNormal;
 
-uniform float uProgress;
+varying vec3 vPos;
+uniform vec3 uColor[3];
+uniform float uLightness;
+uniform float uC1r;
+uniform float uC1g;
+uniform float uC1b;
+uniform float uC2r;
+uniform float uC2g;
+uniform float uC2b;
+uniform float uC3r;
+uniform float uC3g;
+uniform float uC3b;
+
 
 void main() {
-    vec3 rgb = vec3(0., 1., uProgress);
-    gl_FragColor = vec4(rgb, 1.);
 
+//-------- basic gradient ------------
+//   vec3 color1 = vec3(uC1r, uC1g, uC1b);
+//   vec3 color2 = vec3(uC2r, uC2g, uC2b);
+//   vec3 color3 = vec3(uC3r, uC3g, uC3b);
+//   vec3 color1 = vec3(0.03, 0.53, 0.72);
+//   vec3 color2 = vec3(0.03, 0.1, 0.8);
+//   vec3 color3 = vec3(0.94, 0.59, 0.47);
+
+    // color1 = uColor[0];
+    // color2 = uColor[1];
+    // color3 = uColor[2];
+
+
+    float clearcoat = 1.0;
+    float clearcoatRoughness = 0.5;
+
+    // vec4 diffuseColor = vec4(
+    //     mix(mix(color1, color2, smoothstep(-3.0, 3.0, vPos.x)), color3, vPos.z),
+    //     1);
+    
+    vec4 diffuseColor = vec4(
+        mix(mix(uColor[0], uColor[1], smoothstep(-3.0, 3.0, vPos.x)), uColor[2], vPos.z),
+        1);
+
+    gl_FragColor = vec4(diffuseColor.rgb+uLightness, 1.);
+  // gl_FragColor가 fragment shader를 통해 나타나는 최종값으로, diffuseColor에서
+  // 정의한 그라디언트 색상 위에 반사나 빛을 계산한 값을 최종값으로 정의.
+  // gl_FragColor = vec4(mix(mix(color1, color3, smoothstep(-3.0, 3.0,vPos.x)),
+  // color2, vNormal.z), 1.0); 위처럼 최종값을 그라디언트 값 자체를 넣으면 환경
+  // 영향없는 그라디언트만 표현됨.
 }
-
